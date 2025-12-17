bdfa40e5932d3c3413d3bb133cf874f0ff2d74dd
oluwalonimiadedun-crypto <oluwalonimiadedun@gmail.com>
2025-12-17 11:44:25 +0200

    Implement addItem function to post new menu item

--- public/src/addMenuItem.js
+++ public/src/addMenuItem.js
@@ -1,1 +1,11 @@
-Rename file to .js
+function addItem() {
+  $.post("/api/v1/menuItem/new", {
+    name: $("#name").val(),
+    category: $("#category").val(),
+    description: $("#description").val(),
+    price: $("#price").val()
+  }, () => {
+    alert("Item added");
+    window.location.href = "/menuItems";
+  });
+}
