2d5ab8b374b38d9d879b29a571f37c9ce63d9254
oluwalonimiadedun-crypto <oluwalonimiadedun@gmail.com>
2025-12-17 19:07:40 +0200

    Implement login form submission with AJAX

--- public/src/login.js
+++ public/src/login.js
@@ -1,1 +1,7 @@
-Rename file to .js
+$("#loginForm").submit(function(e){
+  e.preventDefault();
+  $.post("/api/v1/user/login", {
+    email: $("#email").val(),
+    password: $("#password").val()
+  }, () => window.location.href = "/dashboard");
+});
