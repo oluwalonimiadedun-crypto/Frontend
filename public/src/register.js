96c228a96a0a3fd66bb59cc379ff8f5fa9a05994
oluwalonimiadedun-crypto <oluwalonimiadedun@gmail.com>
2025-12-17 19:22:51 +0200

    Update register.js

--- public/src/register.js
+++ public/src/register.js
@@ -1,1 +1,14 @@
-Rename file to .js
+$("#registerForm").submit(function (e) {
+  e.preventDefault();
+
+  const data = {
+    name: $("#name").val(),
+    email: $("#email").val(),
+    password: $("#password").val()
+  };
+
+  $.post("/api/v1/user", data, function () {
+    alert("Registration successful!");
+    window.location.href = "/";
+  });
+});
