0b9f9d91521d0dc19f4a3e160ee0206e836ef711
oluwalonimiadedun-crypto <oluwalonimiadedun@gmail.com>
2025-12-17 19:21:20 +0200

    Update myOrders.js

--- public/src/myOrders.js
+++ public/src/myOrders.js
@@ -1,1 +1,19 @@
-Rename file to .js
+$(document).ready(function () {
+  $.get("/api/v1/order/myOrders", function (orders) {
+    let html = "";
+    if (orders.length === 0) {
+      html = "<p>No orders found.</p>";
+    } else {
+      orders.forEach(o => {
+        html += `
+          <div>
+            <h4>Order #${o.id}</h4>
+            <p>Status: ${o.status}</p>
+          </div>
+          <hr/>
+        `;
+      });
+    }
+    $("#orders").html(html);
+  });
+});
