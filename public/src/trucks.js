ae905ebbeaa6e22302422eb18e97732c3bf2470c
oluwalonimiadedun-crypto <oluwalonimiadedun@gmail.com>
2025-12-17 19:23:52 +0200

    Update trucks.js

--- public/src/trucks.js
+++ public/src/trucks.js
@@ -1,1 +1,16 @@
-Rename file to .js
+$(document).ready(function () {
+  $.get("/api/v1/trucks/view", function (trucks) {
+    let html = "";
+    trucks.forEach(t => {
+      html += `
+        <div>
+          <h3>${t.name}</h3>
+          <p>Status: ${t.status}</p>
+          <a href="/truckMenu/${t.id}">View Menu</a>
+        </div>
+        <hr/>
+      `;
+    });
+    $("#truckList").html(html);
+  });
+});
